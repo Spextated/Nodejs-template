919fc29708438cb80874e108d079ead5a224fa63
Spextated <89221713+Spextated@users.noreply.github.com>
2024-04-06 13:27:57 -0700

    Update hunt.js

--- commands/testing/hunt.js
+++ commands/testing/hunt.js
@@ -13,13 +13,12 @@ module.exports = {
     const userData = await db.get(interaction.user.id);
     const creatures = ['Zombie', 'Skeleton', 'Spider', 'Slime', 'Ghost', 'Bear', 'Vampire', 'Werewolf', 'Unicorn', 'Dragon', 'Goblin', 'Gnome', 'Fairy'];
     
-    let damageChance = Math.floor(Math.random() * 5) + 5;
+    let damageChance = (Math.floor(Math.random() * 5) + 5) * userData.rank.level;
     const creature = creatures[Math.floor(Math.random() * creatures.length)];
     const defense = 5 * userData.rank.level;
     const level = userData.rank.level;
     const health = parseInt((100 * 1.15) * level) + 1;
-    const fakeDamage = (damageChance * level);
-    const damage = parseInt(fakeDamage / ((userData.defense + 100) / 100));
+    const damage = parseInt(damageChance / ((userData.defense + 100) / 100));
 
 if (!userData.items.some(item => item.name.includes('⚔️'))) {
   const embed = new EmbedBuilder()
